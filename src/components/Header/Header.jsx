d9a5182321bc4b38bff222c630996a274908b936
Eduardos13 <edik.kozelsky@gmail.com>
2024-10-30 22:30:14 -0400

    2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import s from './Header.module.css';
 
 const Header = () => {
   return (
-    <header className={s.header}>
-      <div className={s.container}>
-        <h1>test</h1>
-        <svg className={s.burgerMenu} width="24" height="24">
-          <use href="../../icons/icons.svg#icon-burger-menu" />
-        </svg>
-      </div>
+    <header>
+      <h1>test header</h1>
     </header>
   );
 };
--- src/pages/ScreensPage/ScreensPage.jsx
+++ src/pages/ScreensPage/ScreensPage.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import s from './ScreensPage.module.css';
+import { Link } from 'react-router-dom';
 
 const ScreensPage = () => {
   return (
     <div>
       <div className={s.container}>
+        <button className={s.filtersBtn}>
+          <svg width="16" height="16">
+            <use href="../../icons/icons.svg#con-filter"></use>
+          </svg>
+          Filters
+        </button>
         <div className={s.startTitle}>
           <p>
             Before starting your project, it is essential to
-            <a>create a board</a> to visualize and track all the necessary tasks
-            and milestones. This board serves as a powerful tool to organize the
-            workflow and ensure effective collaboration among team members.
+            <Link className={s.link}> create a board</Link> to visualize and
+            track all the necessary tasks and milestones. This board serves as a
+            powerful tool to organize the workflow and ensure effective
+            collaboration among team members.
           </p>
         </div>
       </div>
