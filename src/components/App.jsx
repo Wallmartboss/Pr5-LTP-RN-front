d7748ad62c62d2be2ab3267617fe858c6bb0cee9
LiudmylaKobryn <133100987+LiudmylaKobryn@users.noreply.github.com>
2024-11-05 14:45:53 +0100

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import WelcomePage from '../pages/WelcomePage/WelcomePage';
-// import AuthPage from '../pages/AuthPage/AuthPage';
+import WelcomePage from '../pages/WelcomePage/WelcomePage';
+import AuthPage from '../pages/AuthPage/AuthPage';
 import HomePage from '../pages/HomePage/HomePage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<WelcomePage />} />
-        <Route path="/auth/:id" element={<AuthPage />} /> */}
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/auth/:id" element={<AuthPage />} />
         <Route path="/home/*" element={<HomePage />} />
       </Routes>
     </Router>
