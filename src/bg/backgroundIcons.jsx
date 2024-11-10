4e8626bac2d11d284652614df5d5fe57899ce993
LiudmylaKobryn <133100987+LiudmylaKobryn@users.noreply.github.com>
2024-11-10 16:25:24 +0100

    Add backgrounds

---
+++ src/bg/backgroundIcons.jsx
@@ -0,0 +1,37 @@
+import bg1 from './bg-1.png';
+import bg2 from './bg-2.png';
+import bg3 from './bg-3.png';
+import bg4 from './bg-4.png';
+import bg5 from './bg-5.png';
+import bg6 from './bg-6.png';
+import bg7 from './bg-7.png';
+import bg8 from './bg-8.png';
+import bg9 from './bg-9.png';
+import bg10 from './bg-10.png';
+import bg11 from './bg-11.png';
+import bg12 from './bg-12.png';
+import bg13 from './bg-13.png';
+import bg14 from './bg-14.png';
+import bg15 from './bg-15.png';
+import bgDef from './default-bg.png';
+
+const backgroundIcons = [
+  { src: bg1, alt: 'Background 1' },
+  { src: bg2, alt: 'Background 2' },
+  { src: bg3, alt: 'Background 3' },
+  { src: bg4, alt: 'Background 4' },
+  { src: bg5, alt: 'Background 5' },
+  { src: bg6, alt: 'Background 6' },
+  { src: bg7, alt: 'Background 7' },
+  { src: bg8, alt: 'Background 8' },
+  { src: bg9, alt: 'Background 9' },
+  { src: bg10, alt: 'Background 10' },
+  { src: bg11, alt: 'Background 11' },
+  { src: bg12, alt: 'Background 12' },
+  { src: bg13, alt: 'Background 13' },
+  { src: bg14, alt: 'Background 14' },
+  { src: bg15, alt: 'Background 15' },
+  { src: bgDef, alt: 'Default' },
+];
+
+export default backgroundIcons;
