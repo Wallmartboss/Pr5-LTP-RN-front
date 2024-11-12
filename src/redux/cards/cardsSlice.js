fb101fcccfedf40606ee7a5103d2381785d7e7dc
Yuri <wall.mart.boss@proton.me>
2024-11-12 15:17:13 +0200

    Update cardsSlice.js

--- src/redux/cards/cardsSlice.js
+++ src/redux/cards/cardsSlice.js
@@ -12,16 +12,8 @@ const cardsSlice = createSlice({
     cardIdToDelete: null,
   },
   reducers: {
-    openDropdown: (state, action) => {
-      const {cardId} = action.payload;
-      state.openDropdowns[cardId] = true; 
-  },
-  closeDropdown: (state, action) => {
+    toggleDropdown: (state, action) => {
       const cardId = action.payload;
-
-      state.openDropdowns[cardId] = false; 
-  },
-
       state.openDropdowns = Object.keys(state.openDropdowns).reduce(
         (acc, key) => {
           acc[key] = false;
@@ -35,7 +27,6 @@ const cardsSlice = createSlice({
       const cardId = action.payload;
       state.openDropdowns[cardId] = false;
     },
-
 
     toggleDescription(state, action) {
       state.expandedCardId =
@@ -56,78 +47,6 @@ const cardsSlice = createSlice({
   // extraReducers: builder => {
   //   builder;
 
-
-        state.items = [
-          ...state.items,
-          ...newCards.filter(
-            newCard =>
-              !state.items.some(
-                existingCard => existingCard._id === newCard._id
-              )
-          ),
-        ];
-      })
-      .addCase(fetchCards.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
-      })
-      .addCase(addCard.pending, state => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(addCard.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.items.push(action.payload);
-        console.log('Updated Cards:', action.payload); // Логування оновленого масиву карток
-      })
-      .addCase(editCard.pending, state => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(editCard.fulfilled, (state, action) => {
-        state.isLoading = false;
-        const index = state.items.findIndex(
-          card => card._id === action.payload._id
-        );
-        if (index !== -1) state.items[index] = action.payload;
-      })
-      .addCase(editCard.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      })
-      .addCase(deleteCard.pending, state => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(deleteCard.fulfilled, (state, action) => {
-        const cardId = action.meta.arg.cardId;
-        state.items = state.items.filter(card => card._id !== cardId);
-        state.loading = false;
-      })
-      .addCase(deleteCard.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
-      })
-      .addCase(moveCard.pending, state => {
-        state.status = 'loading';
-      })
-      .addCase(moveCard.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        const { cardId, columnId } = action.payload;
-        const index = state.items.findIndex(card => card._id === cardId);
-        if (index !== -1) {
-          state.items[index].columnId = columnId;
-        }
-      })
-      .addCase(moveCard.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.error.message;
-      });
-  },
-});
-
-// export const {
-
   // .addCase(addCard.fulfilled, (state, action) => {
   //   state.isLoading = false;
   //   state.items = state.items.push(action.payload);
@@ -135,21 +54,17 @@ const cardsSlice = createSlice({
   // })
   // Додавання картки
   // },
-//});
+});
 
 export const {
   // addCard,
   toggleDropdown,
-
   toggleDescription,
   openModal,
   closeModal,
-  openDropdown,
   closeDropdown,
 } = cardsSlice.actions;
 
-
-export const selectCardIdToDelete = (state) => state.cards.cardIdToDelete;
-
+export const selectCardIdToDelete = state => state.cards.cardIdToDelete;
 
 export default cardsSlice.reducer;
