e7c56b8d1971529abbcfc72a85243a3fea65cf75
Yuri <wall.mart.boss@proton.me>
2024-11-12 17:56:01 +0200

    upd CardList

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,31 +1,39 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import s from './CardList.module.css';
 import {
   selectCardsByColumnId,
   selectColumnsByBoardId,
-
 } from '../../redux/columns/selectors.js';
 import { selectSelectedBoard } from '../../redux/boards/selectors.js';
 import { moveCard } from '../../redux/cards/operations.js';
 
 import Card from '../Card/Card.jsx';
-
-
+import { toggleDropdown } from '../../redux/cards/cardsSlice.js';
+import {
+  selectAllPriorityFilter,
+  selectSelectAll,
+} from '../../redux/filters/selectors';
 
 const CardList = ({ columnId }) => {
-
+  const dispatch = useDispatch();
   const cards = useSelector(state => selectCardsByColumnId(state, columnId));
+  // Отримуємо інформацію про фільтри пріоритету
+  const priorityFilter = useSelector(selectAllPriorityFilter);
+  const selectAll = useSelector(selectSelectAll);
+
+  // Фільтруємо картки за пріоритетом
+  const filteredCards = selectAll
+    ? cards
+    : cards.filter(card => priorityFilter[card.priority]);
   const selectedBoard = useSelector(selectSelectedBoard);
   const boardId = selectedBoard?._id;
-  const columns = useSelector(state => {
-    return selectColumnsByBoardId(state, boardId);
+  const columns = useSelector(state => selectColumnsByBoardId(state, boardId));
 
-    const filteredColumns = columns.filter(column => column._id !== columnId);
-
-  });
   const filteredColumns = columns.filter(column => column._id !== columnId);
+
   const openDropdowns = useSelector(state => state.cards.openDropdowns);
-  // console.log('Cards for column:', columnId, cards);
+  console.log('All cards for column:', cards);
+  console.log('Filtered cards for column:', filteredCards);
   const handleMoveCard = (newColumnId, cardId) => {
     if (cardId && newColumnId && newColumnId !== columnId) {
       dispatch(moveCard({ cardId, columnId: newColumnId, boardId }));
@@ -37,7 +45,6 @@ const CardList = ({ columnId }) => {
     dispatch(toggleDropdown(cardId));
   };
 
-
   return (
     <div className={s.cardsContainer}>
       {cards.length === 0 ? (
@@ -47,13 +54,11 @@ const CardList = ({ columnId }) => {
           <Card
             key={card._id || `card-${index}`}
             card={card}
-
             columnId={columnId}
             handleMoveCard={handleMoveCard}
             openDropdowns={openDropdowns}
-
             filteredColumns={filteredColumns}
-  
+            toggleDropdown={toggleDropdownHandler}
           />
         ))
       )}
